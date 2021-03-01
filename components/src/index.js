b19eb83c276920cd865d1441fb1a83f79eff65b5
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-01 17:23:59 -0600

    feat: added approvalCard componenet

--- components/src/index.js
+++ components/src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 3:34pm"
-                content="Nice blog post!"
-                imgSrc="https://source.unsplash.com/random/150x150"
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 4:49pm"
-                content="Creative and insightful"
-                imgSrc="https://source.unsplash.com/random/150x150"
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 9:31am"
-                content="Cool beans"
-                imgSrc="https://source.unsplash.com/random/150x150"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 3:34pm"
+                    content="Nice blog post!"
+                    imgSrc="https://source.unsplash.com/random/150x150"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 4:49pm"
+                    content="Creative and insightful"
+                    imgSrc="https://source.unsplash.com/random/150x150"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 9:31am"
+                    content="Cool beans"
+                    imgSrc="https://source.unsplash.com/random/150x150"
+                />
+            </ApprovalCard>
         </div>
     );
 };
