bda07cee8b85cc090c0d2002f9353cfeaaf841b7
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-02 15:10:00 -0600

    feat: alternate state initialization

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,33 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // Alternate state initialization instead of a constructor
+    state = { lat: null, errorMessage: '' };
 
-        // this is the only time we do direct assignment to the state property!!!!!!!!!
-        this.state = { lat: null, errorMessage: '' }; // state initialized; eventually we'll get this number, but set to null until that value is determined
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
+    }
+
+    componentDidUpdate() {
+        console.log('Component did update and re-rendered');
     }
 
     // React requires a render() method for each component
     render() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <div>Latitude: {this.state.lat}</div>;
         }
 
-        return <div>Loading!</div>
+        return <div>Loading!</div>;
     }
 }
 
