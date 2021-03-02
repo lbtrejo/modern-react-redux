d3af9729dcba28db1d60f1e9c57cda3c1b264a1c
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-02 09:52:08 -0600

    feat: initializing state via constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null }; // state initialized; eventually we'll get this number, but set to null until that value is determined
+    }
+
+    // React requires a render() method for each component
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
@@ -11,7 +18,7 @@ class App extends React.Component {
                 console.log(err);
             }
         );
-        return <div>Latitude: </div>;
+        return <div>Latitude: </div>; // needs to return a single parent element
     }
 }
 
