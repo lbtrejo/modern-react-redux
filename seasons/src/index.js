941a0d425f1e6daaa14972b82b9e07fe8663c751
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-02 17:05:01 -0600

    feat: added styling and loader component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component {
     // Alternate state initialization instead of a constructor
@@ -23,10 +25,10 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <div>Loading!</div>;
+        return <Loader />;
     }
 }
 
