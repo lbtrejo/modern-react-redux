c74787ac63047b22a85743adbeb33002f54c20ac
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-02 11:58:06 -0600

    feat: conditional rendering of geo component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,20 +5,30 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null }; // state initialized; eventually we'll get this number, but set to null until that value is determined
+        // this is the only time we do direct assignment to the state property!!!!!!!!!
+        this.state = { lat: null, errorMessage: '' }; // state initialized; eventually we'll get this number, but set to null until that value is determined
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
 
     // React requires a render() method for each component
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position);
-            },
-            (err) => {
-                console.log(err);
-            }
-        );
-        return <div>Latitude: </div>; // needs to return a single parent element
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
     }
 }
 
