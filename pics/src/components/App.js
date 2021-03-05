afd75a8d8962e4ec087a6f391af1de1dec152871
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-05 14:12:45 -0600

    feat: axios setup for image fetch

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit = (term) => {
-        console.log(term)
-    }
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        console.log(term);
+        const response = await axios.get(
+            'https://api.unsplash.com/search/photos',
+            {
+                params: {
+                    query: term,
+                },
+                headers: {
+                    Authorization:
+                        'Client-ID 8b04V07fcpQCGOHKMITjWev09iX_FQubMNU8sUHo1gw',
+                },
+            }
+        );
+
+        this.setState({ images: response.data.results });
+    };
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.length} images.
             </div>
         );
     }
