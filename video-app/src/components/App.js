8a5c2a0926f5d5d2f44ff4ea1ccecfcacc672fd8
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-10 14:38:31 -0600

    feat: embedded iframe video player functional

--- video-app/src/components/App.js
+++ video-app/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../api/youtube';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -16,16 +17,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
-    onVideoSelect = async (video) => {
-        console.log('From the App!', video);
-        await this.setState({ selectedVideo: video });
-        console.log(this.state.selectedVideo);
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     videos={this.state.videos}
                     onVideoSelect={this.onVideoSelect}
