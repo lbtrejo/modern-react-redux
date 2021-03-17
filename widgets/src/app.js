5ce3e729eb7a2dc54dea084e9f3cbf9cf58e7c5e
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-16 23:14:55 -0500

    feat: dropdown scaffolded and functional

--- widgets/src/app.js
+++ widgets/src/app.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
@@ -19,10 +20,32 @@ export default () => {
         },
     ];
 
+    const options = [
+        {
+            label: 'The Color Red',
+            value: 'red',
+        },
+        {
+            label: 'The Color Green',
+            value: 'green',
+        },
+        {
+            label: 'A Shade of Blue',
+            value: 'blue',
+        },
+    ];
+
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     );
 };
