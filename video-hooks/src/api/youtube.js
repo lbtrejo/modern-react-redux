6990390a1b08504fc0ff57a49c886b8156a1535c
Levi Trejo <levi.b.trejo@gmail.com>
2021-03-20 20:34:43 -0500

    refactor: search bar functional component

---
+++ video-hooks/src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_YT_API_KEY;
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: API_KEY,
+    },
+});
---
+++ video-hooks/src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loader = ({ message }) => {
+    return (
+        <div className="ui active inverted dimmer">
+            <div className="ui big text loader">{message}</div>
+        </div>
+    );
+};
+
+Loader.defaultProps = {
+    message: 'Loading',
+};
+
+export default Loader;
