e7f47f1106c5b272828b9983a2ec5391acd7ccc8
Levi Trejo <levi.b.trejo@gmail.com>
2021-04-01 22:36:48 -0500

    feat: streams routes and scaffolding done

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-    return <div>Page One</div>;
-};
-
-const PageTwo = () => {
-    return <div>Page Two</div>;
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route
+                        path="/streams/delete"
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
