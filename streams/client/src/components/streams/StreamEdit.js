5fcb566f6fa9cfed08f4badf242e4bce476c3c85
Levi Trejo <levi.b.trejo@gmail.com>
2021-04-13 09:59:52 -0500

    feat: StreamEdit component updated to use StreamForm

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    };
+
     render() {
         console.log(this.props); // the props object becomes available because the router component adds it to its child component on route
-        return <div>StreamEdit</div>;
+
+        if (!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    onSubmit={this.onSubmit}
+                    initialValues={this.props.stream}
+                />
+            </div>
+        );
     }
 }
 
@@ -17,4 +35,6 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+    StreamEdit
+);
