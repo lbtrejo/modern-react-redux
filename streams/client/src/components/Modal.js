1758e9a3bbb0daf635f0b3033d858ea21d0166f0
Levi Trejo <levi.b.trejo@gmail.com>
2021-04-18 22:54:00 -0500

    feat: delete stream modal functional

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -5,23 +5,16 @@ import history from '../history';
 const Modal = (props) => {
     return ReactDOM.createPortal(
         <div
-            onClick={() => {
-                history.push('/');
-            }}
+            onClick={props.onDismiss}
             className="ui dimmer modals visible active"
         >
             <div
                 onClick={(e) => e.stopPropagation()}
                 className="ui standard modal visible active"
             >
-                <div className="header">Delete Stream</div>
-                <div className="content">
-                    Are you sure you want to delete this stream?
-                </div>
-                <div className="actions">
-                    <button className="ui button">Cancel</button>
-                    <button className="ui purple button">Delete</button>
-                </div>
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>,
         document.querySelector('#modal')
