fe3f1f9695bab5d5efe067e6f4646990f3fac347
Levi Trejo <levi.b.trejo@gmail.com>
2021-04-19 20:14:25 -0500

    feat: StreamShow component functional with stream fetching

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamShow = () => {
-    return <div>StreamShow</div>;
+import { fetchStream } from '../../actions';
+
+// 1. Read the ID out of the URL
+// 2. Call an action creator to fetch that particular stream
+// 3. Create a mapStateToProps function to get that stream into the component
+
+class StreamShow extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        const { title, description } = this.props.stream;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id],
+    };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
